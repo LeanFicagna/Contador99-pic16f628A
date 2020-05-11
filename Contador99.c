53915b4d3ea47e19e9a82e1d728069ec33179773
Leanderson Ficagna <64813850+LeanFicagna@users.noreply.github.com>
2020-05-11 18:46:39 -0300

    Update Contador99.c

--- Contador99.c
+++ Contador99.c
@@ -1,9 +1,9 @@
 void main() {
      unsigned char cont = 0x0;
-                   
-     CMCON = 0x07;
-     TRISB = 0x00;
-     PORTB = 0x00;
+     
+     CMCON = 0x07; // Desabilita os comparadores internos
+     TRISB = 0x00; // Configura como saída o PORTB
+     PORTB = 0x00; // Configura o estado inicial do PORTB como baixo
 
      while(1) {
               PORTB = ((cont/10) << 4) + cont % 10;
